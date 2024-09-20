e1f285eb95b15a79105ffc7f9fd6c89299bb4244
Soa1804 <137106233+Soa1804@users.noreply.github.com>
2024-09-20 20:34:03 -0300

    teste

--- Exercicio_1.py
+++ Exercicio_1.py
@@ -1,3 +1,4 @@
 '''1 - Fazer um programa que leia dois números inteiros. Armazene os números lindos nas
 variáveis num1 e num2. Se número o número armazenado na variável num2 for par, exiba a
 soma dos números lidos, se não, exiba a multiplicação.'''
+#dddddd
